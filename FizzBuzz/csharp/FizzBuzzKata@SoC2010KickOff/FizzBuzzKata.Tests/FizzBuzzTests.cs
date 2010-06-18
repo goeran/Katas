6209d113f5efb1f973b1de16e8524cb79ba92e9e
goeran <mail@goeran.no>
2010-06-18 15:14:49 +0200

    1. added gitignore file. 2. Removed old cpp folder 3. added FizzBuzz c# kata from Capgemini Summer of Code 2010 kick-off

---
+++ FizzBuzz/csharp/FizzBuzzKata@SoC2010KickOff/FizzBuzzKata.Tests/FizzBuzzTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace FizzBuzzKata.Tests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        private FizzBuzz fizzBuzz;
+
+        [SetUp]
+        public void Setup()
+        {
+            fizzBuzz = new FizzBuzz();
+        }
+
+        [Test]
+        public void Assure_number_undivisible_by_three_or_five_returns_number()
+        {
+            var numbers = new List<int>() {0, 1};
+
+            foreach (var number in numbers)
+            {
+                var result = fizzBuzz.GetAnswer(number);
+                Assert.AreEqual(Convert.ToString(number), result);
+            }
+            
+        }
+
+        [Test]
+        public void Assure_numbers_divisible_by_3_returns_fizz()
+        {
+            var numbers = new List<int>() { 3,6,9};
+
+            foreach (var number in numbers)
+            {
+                var result = fizzBuzz.GetAnswer(number);
+                Assert.AreEqual("fizz", result);
+            }
+        }
+
+        [Test]
+        public void Assure_numbers_divisible_by_5_returns_buzz()
+        {
+            var numbers = new List<int>() { 5,10,20 };
+
+            foreach (var number in numbers)
+            {
+                var result = fizzBuzz.GetAnswer(number);
+                Assert.AreEqual("buzz", result);
+            }
+        }
+
+        [Test]
+        public void Assure_15_returns_fizzbuzz()
+        {
+            var result = fizzBuzz.GetAnswer(15);
+
+            Assert.AreEqual("fizzbuzz", result);
+        }
+
+        [Test]
+        public void Assure_30_returns_fizzbuzz()
+        {
+            var result = fizzBuzz.GetAnswer(30);
+
+            Assert.AreEqual("fizzbuzz", result);
+        }
+    }
+}
---
+++ FizzBuzz/csharp/FizzBuzzKata@SoC2010KickOff/FizzBuzzKata/FizzBuzz.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzzKata
+{
+    public class FizzBuzz
+    {
+        public String GetAnswer(int number)
+        {
+            if (IsFizzBuzz(number)) return "fizzbuzz";
+            if (IsFizz(number)) return "fizz";
+            if (IsBuzz(number)) return "buzz";
+            
+            return Convert.ToString(number); 
+        }
+
+        private Boolean IsFizz(int number)
+        {
+            return number % 3 == 0 && number != 0;
+        }
+
+        private Boolean IsBuzz(int number)
+        {
+            return number % 5 == 0 && number != 0;
+        }
+
+        private Boolean IsFizzBuzz(int number)
+        {
+            return number % 15 == 0 && number != 0;
+        }
+    }
+}
