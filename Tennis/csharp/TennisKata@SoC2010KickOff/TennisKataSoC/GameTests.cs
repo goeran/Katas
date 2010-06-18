67068ddd640606b3639af25e5fe794bed1c59c88
goeran <mail@goeran.no>
2010-06-18 15:19:29 +0200

    added Tennis kata from Capgemini Summer of Code 2010 kick-off

---
+++ Tennis/csharp/TennisKata@SoC2010KickOff/TennisKataSoC/GameTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace TennisKataSoC
+{
+    [TestFixture]
+    public class GameTests
+    {
+        private TennisGame game;
+
+        [Test]
+        public void Assure_valid_scores()
+        {
+            var table = GetTestTable();
+
+            foreach (var row in table)
+            {
+                game = new TennisGame();
+
+                Play_a_set((int)row[0], (int)row[1]);
+                
+                Assert.AreEqual(row[2], game.GetScore());
+            }
+        }
+
+        public void Play_a_set(int player1Score, int player2Score)
+        {
+            for (int i = 0; i < player1Score; i++)
+            {
+                game.Player1Scores();
+            }
+            for (int i = 0; i < player2Score; i++)
+            {
+                game.Player2Scores();
+            }
+        }
+
+        private List<List<Object>> GetTestTable()
+        {
+            var table = new List<List<Object>>()
+                {
+                    new List<Object>() {0, 0, "love - love"},
+                    new List<Object>() {0, 1, "love - 15"},
+                    new List<Object>() {1, 0, "15 - love"},
+                    new List<Object>() {1, 1, "15 - 15"},
+                    new List<Object>() {0, 2, "love - 30"},
+                    new List<Object>() {1, 2, "15 - 30"},
+                    new List<Object>() {2, 2, "30 - 30"},
+                    new List<Object>() {2, 0, "30 - love"},
+                    new List<Object>() {2, 1, "30 - 15"},
+                    new List<Object>() {0, 3, "love - 40"},
+                    new List<Object>() {1, 3, "15 - 40"},
+                    new List<Object>() {2, 3, "30 - 40"},
+                    new List<Object>() {3, 3, "deuce"},
+                    new List<Object>() {3, 0, "40 - love"},
+                    new List<Object>() {3, 1, "40 - 15"},
+                    new List<Object>() {3, 2, "40 - 30"},
+                    new List<Object>() {0, 4, "player2 has won"},
+                    new List<Object>() {4, 4, "deuce"},
+                    new List<Object>() {4, 0, "player1 has won"},
+                    new List<Object>() {4, 3, "advantage player1"},
+                    new List<Object>() {14, 15, "advantage player2"}
+
+                };
+            return table;
+        }
+    }
+}
---
+++ Tennis/csharp/TennisKata@SoC2010KickOff/TennisKataSoC/TennisGame.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TennisKataSoC
+{
+    public class TennisGame
+    {
+        private int player1Score = 0;
+        private int player2Score = 0;
+
+        public String GetScore()
+        {
+            if (IsWinPlayer1()) return "player1 has won";
+
+            if (IsWinPlayer2()) return "player2 has won";
+
+            if (IsAdvantagePlayer2()) return "advantage player2";
+
+            if (IsAdvantagePlayer1()) return "advantage player1";
+            
+            if (IsDeuce()) return "deuce";
+            return ConvertScoreToString(player1Score) + " - " + ConvertScoreToString(player2Score);
+        }
+
+        private bool IsWinPlayer1()
+        {
+            return player1Score >= player2Score + 2 && player1Score > 3;
+        }
+
+        private bool IsWinPlayer2()
+        {
+            return player2Score >= player1Score + 2 && player2Score > 3;
+        }
+
+        private bool IsAdvantagePlayer2()
+        {
+            return player2Score == player1Score+1 && player2Score > 3;
+        }
+
+        private bool IsAdvantagePlayer1()
+        {
+            return player1Score == player2Score+1 && player1Score > 3;
+        }
+
+        private bool IsDeuce()
+        {
+            return player1Score == player2Score && player1Score > 2;
+        }
+
+        public void Player1Scores()
+        {
+            player1Score++;
+        }
+
+        public void Player2Scores()
+        {
+           player2Score++;
+        }
+
+        private String ConvertScoreToString(int score)
+        {
+            var values = new List<String>{"love", "15", "30", "40"};
+            return values[score];
+        }
+    }
+}
