5ef08303a162e476852922d8c294b06ec2bafc6e
goeran <mail@goeran.no>
2010-09-15 21:46:31 +0200

    2nd try of Tennis kata in c#

---
+++ Tennis/csharp/2ndTry/TennisKata/Tennis.cs
@@ -0,0 +1,75 @@
+﻿namespace TennisKata
+{
+    public class Tennis
+    {
+        private int winsPlayer1, winsPlayer2;
+
+        public string PrintScore()
+        {
+            if (OnePlayerLeadFortyToLove())
+                return LeadingPlayer() + " wins";
+
+            if (BothPlayersHasWonMoreThanThreeBalls() && ScoreIsEqual())
+                return "deuce";
+
+            if (BothPlayersHasWonMoreThanThreeBalls() && APlayerLeadByOneBall())
+                return "advantage " + LeadingPlayer();
+
+            return string.Format("{0} - {1}", Score(winsPlayer1), Score(winsPlayer2));
+        }
+
+        private bool OnePlayerLeadFortyToLove()
+        {
+            return OnePlayerHasForty() && OnePlayerHasLove();
+        }
+
+        private bool OnePlayerHasLove()
+        {
+            return winsPlayer1 == 0 || winsPlayer2 == 0;
+        }
+
+        private bool OnePlayerHasForty()
+        {
+            return winsPlayer1 == 3 || winsPlayer2 == 3;
+        }
+
+        private string LeadingPlayer()
+        {
+            return winsPlayer1 > winsPlayer2 ? "Player1" : "Player2";
+        }
+
+        private bool BothPlayersHasWonMoreThanThreeBalls()
+        {
+            return winsPlayer1 > 2 && winsPlayer2 > 2;
+        }
+
+        private bool ScoreIsEqual()
+        {
+            return winsPlayer1 == winsPlayer2;
+        }
+
+        private bool APlayerLeadByOneBall()
+        {
+            return winsPlayer1 == winsPlayer2 + 1 || winsPlayer2 == winsPlayer1 + 1;
+        }
+
+        private static string Score(int balls)
+        {
+            if (balls == 1) return "fifteen";
+            if (balls == 2) return "thirty";
+            if (balls == 3) return "forty";
+            
+            return "love";
+        }
+
+        public void Player1WinsTheBall()
+        {
+            winsPlayer1++;
+        }
+
+        public void Player2WinsTheBall()
+        {
+            winsPlayer2++;
+        }
+    }
+}
---
+++ Tennis/csharp/2ndTry/TennisKata/Tennis_Tests.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace TennisKata
+{
+    [TestFixture]
+    public class When_Playing
+    {
+        private Tennis tennis;
+
+        [Test]
+        public void Then_assure_score_is_printed_correctly()
+        {
+            var games = new List<object[]>();
+            games.Add(new object[]{ 0, 0, "love - love" });
+            games.Add(new object[]{ 1, 0, "fifteen - love" });
+            games.Add(new object[]{ 0, 1, "love - fifteen" });
+            games.Add(new object[]{ 1, 1, "fifteen - fifteen" });
+            games.Add(new object[]{ 2, 0, "thirty - love" });
+            games.Add(new object[]{ 2, 1, "thirty - fifteen" });
+            games.Add(new object[]{ 2, 2, "thirty - thirty" });
+            games.Add(new object[]{ 3, 0, "Player1 wins" });
+            games.Add(new object[]{ 0, 3, "Player2 wins" });
+            games.Add(new object[]{ 3, 1, "forty - fifteen" });
+            games.Add(new object[]{ 3, 3, "deuce" });
+            games.Add(new object[]{ 4, 3, "advantage Player1" });
+            games.Add(new object[]{ 5, 4, "advantage Player1" });
+            games.Add(new object[]{ 3, 4, "advantage Player2" });
+            games.Add(new object[]{ 4, 5, "advantage Player2" });
+            games.Add(new object[]{ 5, 5, "deuce" });
+
+
+            foreach (var game in games)
+            {
+                Play((int)game[0], (int)game[1], (string)game[2]);
+            }
+        }
+
+        private void Play(int ballsToPlayer1, int ballsToPlayer2, string expectedScore)
+        {
+            tennis = new Tennis();
+
+            for (int i = 0; i < ballsToPlayer1; i++)
+                tennis.Player1WinsTheBall();
+               
+            for (int i = 0; i < ballsToPlayer2; i++)
+                tennis.Player2WinsTheBall();
+
+            Assert.AreEqual(expectedScore, tennis.PrintScore());
+        }
+    }
+}
