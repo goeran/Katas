7f39378006f66c6fe001cd8d2af689d6545045f8
Gøran Hansen <mail@goeran.no>
2010-09-23 22:57:05 +0200

    added parameterize tests to be more DRY:)

--- Tennis/ruby/FirstTry/tennis_spec.rb
+++ Tennis/ruby/FirstTry/tennis_spec.rb
@@ -3,37 +3,27 @@ require 'spec'
 require 'tennis'
 
 describe Tennis, "score" do
+  @games = [
+    {:player1 => 0, :player2 => 0, :result => "love - love"},
+    {:player1 => 1, :player2 => 0, :result => "fifteen - love"},
+    {:player1 => 0, :player2 => 1, :result => "love - fifteen"},
+    {:player1 => 1, :player2 => 1, :result => "fifteen - fifteen"},
+    {:player1 => 2, :player2 => 0, :result => "thirty - love"},
+    {:player1 => 2, :player2 => 1, :result => "thirty - fifteen"},
+    {:player1 => 2, :player2 => 2, :result => "thirty - thirty"},
+    {:player1 => 0, :player2 => 2, :result => "love - thirty"},
+    {:player1 => 1, :player2 => 2, :result => "fifteen - thirty"},
+    ]    
+
   before :each do
     @tennis = Tennis.new
   end
   
-  it "should be 'love - love' when game starts" do
-    @tennis.score.should eql "love - love"
-  end
-  
-  it "should be 'fifteen - love' when player 1 has won the first ball" do
-    @tennis.player1_wins_ball
-    @tennis.score.should eql "fifteen - love"
-  end
-  
-  it "should be 'love - fifteen' when player 2 has won the first ball" do
-    @tennis.player2_wins_ball
-    @tennis.score.should eql "love - fifteen"
-  end
-  
-  it "should be 'fifteen - fifteen' when both players has won 1 ball each" do
-    @tennis.player1_wins_ball
-    @tennis.player2_wins_ball
-    @tennis.score.should eql "fifteen - fifteen"
-  end
-  
-  it "should be 'thirty - love' when player 1 has won two balls" do
-    @tennis.player1_wins 2
-    @tennis.score.should eql "thirty - love"
-  end
-  
-  it "should be 'love - thirty' when player 2 has won two balls" do
-    @tennis.player2_wins 2
-    @tennis.score.should eql "love - thirty"
+  @games.each do |game|
+    it "should be '#{game[:result]}' when #{game[:player1]} - #{game[:player2]}" do
+      @tennis.player1_wins game[:player1]
+      @tennis.player2_wins game[:player2]
+      @tennis.score.should eql game[:result]
+    end
   end
 end
