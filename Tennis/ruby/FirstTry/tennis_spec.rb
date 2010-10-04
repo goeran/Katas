6ea47592f9b995db264fe3e2375ccb17ee13a6e3
Gøran Hansen <mail@goeran.no>
2010-10-04 16:50:43 +0200

    refactored the tests

--- Tennis/ruby/FirstTry/tennis_spec.rb
+++ Tennis/ruby/FirstTry/tennis_spec.rb
@@ -43,69 +43,91 @@ describe Tennis, "Deuce" do
     @tennis = Tennis.new
   end
   
-  it "is deuce when both players has forty and score is equal" do
-    @tennis.player1_wins 3
-    @tennis.player2_wins 3
-    @tennis.score.should eql "deuce"
+  context "when 'forty - forty'" do
+    it "is deuce" do
+      @tennis.player1_wins 3
+      @tennis.player2_wins 3
+      @tennis.score.should eql "deuce"
+    end
+  end
+  
+  context "When both players has won more than 3 balls each and score is equal" do
+    it "is deuce" do
+      @tennis.player1_wins 6
+      @tennis.player2_wins 6
+      @tennis.score.should eql "deuce"
+    end
   end
 end
 
 describe Tennis, "Advantage" do
   before :each do @tennis = Tennis.new end
   
-  it "is advantage when deuce and Player 1 leads by one ball" do
-    @tennis.player1_wins 4
-    @tennis.player2_wins 3
-    @tennis.score.should eql "advantage Player 1"
-  end
-  
-  it "is advantage when deuce and Player 2 leads by one ball" do
-    @tennis.player1_wins 3
-    @tennis.player2_wins 4
-    @tennis.score.should eql "advantage Player 2"
-  end 
+  context "When deuce" do
+    context "and Player 1 leads by one ball" do
+      it "is advantage to Player 1" do
+        @tennis.player1_wins 4
+        @tennis.player2_wins 3
+        @tennis.score.should eql "advantage Player 1"
+      end
+    end
+    
+    context "and Player 2 leads by one ball" do
+      it "is advantage to Player 2" do
+        @tennis.player1_wins 3
+        @tennis.player2_wins 4
+        @tennis.score.should eql "advantage Player 2"
+      end
+    end
+  end  
 end
 
 describe Tennis, "Win" do
   before :each do @tennis = Tennis.new end
   
-  context "when Player 1 leads forty - love" do
-    it "is Player1 that wins if he wins the next ball" do
-      @tennis.player1_wins 3
-      @tennis.score.should eql "forty - love"
+  context "when 'forty - love'" do
+    context "and Player 1 wins the next ball" do
+      it "is Player 1 that wins" do
+        @tennis.player1_wins 3
+        @tennis.score.should eql "forty - love"
 
-      @tennis.player1_wins 1
-      @tennis.score.should eql "Player 1 wins"
+        @tennis.player1_wins 1
+        @tennis.score.should eql "Player 1 wins"
+      end
+    end
+    
+    context "and Player 2 wins the next ball" do
+      it "is Player 2 that wins" do
+        @tennis.player2_wins 3
+        @tennis.score.should eql "love - forty"
+
+        @tennis.player2_wins 1
+        @tennis.score.should eql "Player 2 wins"
+      end
     end
   end
   
-  context "when Player 2 leads forty - love" do
-    it "is Player2 that wins if he wins the next ball" do
-      @tennis.player2_wins 3
-      @tennis.score.should eql "love - forty"
-
-      @tennis.player2_wins 1
-      @tennis.score.should eql "Player 2 wins"
-    end
-  end
-    
   context "when deuce" do
-    it "is Player 1 that wins if he win 2 balls in a row" do
-      @tennis.player1_wins 3
-      @tennis.player2_wins 3
-      @tennis.score.should eql "deuce"
+    context "and Player 1 wins two balls in a row" do
+      it "is Player 1 that wins" do
+        @tennis.player1_wins 3
+        @tennis.player2_wins 3
+        @tennis.score.should eql "deuce"
 
-      @tennis.player1_wins 2
-      @tennis.score.should eql "Player 1 wins"
+        @tennis.player1_wins 2
+        @tennis.score.should eql "Player 1 wins"
+      end
     end
+    
+    context "and Player 2 wins two balls in a row" do
+      it "is Player 2 that wins" do
+        @tennis.player1_wins 4
+        @tennis.player2_wins 4
+        @tennis.score.should eql "deuce"
 
-    it "is Player2 wins if he win 2 balls in a row" do
-      @tennis.player1_wins 4
-      @tennis.player2_wins 4
-      @tennis.score.should eql "deuce"
-
-      @tennis.player2_wins 2
-      @tennis.score.should eql "Player 2 wins"
+        @tennis.player2_wins 2
+        @tennis.score.should eql "Player 2 wins"
+      end
     end
   end
 end
