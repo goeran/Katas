50e26a3230763ce1b6a5c3e1613922eda03f00f4
gohans <gohans@statoil.com>
2010-11-30 11:48:15 +0100

    completed the tennis kata. next step is to branch for the different stages

--- Tennis/ruby/StatoilGathering/lib/tennis.rb
+++ Tennis/ruby/StatoilGathering/lib/tennis.rb
@@ -5,16 +5,49 @@ class Tennis
   def initialize
     @balls_player_one = 0
     @balls_player_two = 0
+    @score = { 0 => :love, 1 => :fifteen, 2 => :thirty, 3 => :forty }
   end
 
   def score
+    return "#{leading_player} wins" if one_player_has_more_than :forty and leading_player_leads_by 2
+    return "deuce" if both_players_have :forty and score_is_equal
+    return "advantage #{leading_player}" if both_players_have :forty and leading_player_leads_by 1
     "#{pretty @balls_player_one} - #{pretty @balls_player_two}"
   end
 
+  def one_player_has_more_than score
+    score = 3 if score == :forty
+    @balls_player_one > score or @balls_player_two > score
+  end
+
+  def one_player_has score
+    score = 0 if score == :love
+    score = 1 if score == :fifteen
+    score = 2 if score == :thirty
+    @balls_player_one == score or @balls_player_two == score
+  end
+
+  def score_is_equal
+    @balls_player_one == @balls_player_two
+  end
+
+  def leading_player_leads_by balls
+    @balls_player_one >= @balls_player_two + balls or
+      @balls_player_two >= @balls_player_one + balls
+  end
+
+  def leading_player
+    return "player one" if @balls_player_one > @balls_player_two
+    "player two"
+  end
+
+  def both_players_have score
+    score = 3 if score == :forty
+    @balls_player_one >= score and @balls_player_two >= score
+  end
+
   def pretty balls
-    return "love" if balls == 0
-    return "fifteen" if balls == 1
-    return "thirty" if balls == 2
+    @score[balls].to_s
   end
 
   def score_player_one
@@ -23,5 +56,10 @@ class Tennis
 
   def score_player_two
     @balls_player_two += 1
+  end
+
+  def play p1, p2
+    p1.times { score_player_one }
+    p2.times { score_player_two }
   end
 end
--- Tennis/ruby/StatoilGathering/spec/tennis_spec.rb
+++ Tennis/ruby/StatoilGathering/spec/tennis_spec.rb
@@ -3,29 +3,78 @@
 
 require 'tennis'
 
+def play(p1, p2, result)
+  { :p1 => p1, :p2 => p2, :result => result }
+end
+
+def test game
+  it "should be '#{game[:result]}' when #{game[:p1]}-#{game[:p2]}" do
+        @tennis.play(game[:p1], game[:p2])
+        @tennis.score.should eql game[:result]
+  end
+end
+
 describe Tennis do
   before(:each) do
     @tennis = Tennis.new
   end
 
   describe "score" do
-    it "should be 'love - love' when 0-0" do
-      @tennis.score.should eql "love - love"
+    [
+      play(0, 0, "love - love"),
+      play(1, 0, "fifteen - love"),
+      play(0, 1, "love - fifteen"),
+      play(2, 0, "thirty - love"),
+      play(2, 1, "thirty - fifteen"),
+      play(2, 2, "thirty - thirty"),
+      play(3, 0, "forty - love"),
+      play(3, 2, "forty - thirty"),
+    ].each do |game|
+      test game
     end
+  end
 
-    it "should be 'fifteen - love' when 1-0" do
-      @tennis.score_player_one
-      @tennis.score.should eql "fifteen - love"
+  describe "deuce" do
+    [
+      play(3, 3, "deuce"),
+      play(4, 4, "deuce"),
+      play(5, 5, "deuce"),
+    ].each do |game|
+      test game 
     end
+  end
 
-    it "should be 'love - fifteen' when 0-1" do
-      @tennis.score_player_two
-      @tennis.score.should eql "love - fifteen"
+  describe "advantage" do
+    [
+      play(4, 3, "advantage player one"),
+      play(3, 4, "advantage player two"),
+      play(5, 4, "advantage player one"),
+      play(9, 10, "advantage player two"),
+    ].each do |game|
+      test game
     end
+  end
 
-    it "should be 'thirty - love' when 2-0" do
-      2.times { @tennis.score_player_one }
-      @tennis.score.should eql "thirty - love"
+  describe "win" do
+    [
+      play(4, 0, "player one wins"),
+      play(0, 4, "player two wins"),
+      play(4, 1, "player one wins"),
+      play(1, 4, "player two wins"),
+      play(4, 2, "player one wins"),
+      play(2, 4, "player two wins")
+    ].each do |game|
+      test game
+    end
+  end
+
+  describe "win after advantage" do
+    [
+      play(5, 3, "player one wins"),
+      play(10, 8, "player one wins"),
+      play(10, 12, "player two wins"),
+    ].each do |game|
+      test game
     end
   end
 end
